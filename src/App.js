196d338540f0a3c22ca81dcf01678203a8396708
Marivaldo Cavalheiro <marivaldo@gmail.com>
2022-05-29 18:53:48 +0100

    task: CardList Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -51,7 +53,7 @@ class App extends Component {
           placeholder='search monsters'
           onChange={onSearchChange}
         />
-        {
+        { /*
           filteredMonsters.map((monster) => {
             return(
               <div key={monster.id}>
@@ -59,7 +61,8 @@ class App extends Component {
               </div>
             );
           })
-        }
+          */}
+        <CardList />
       </div>
     );
   }
