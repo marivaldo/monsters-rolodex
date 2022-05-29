5e2fcc15708cfcb730fd0a290b1cbe8e08b0b10c
Marivaldo Cavalheiro <marivaldo@gmail.com>
2022-05-29 16:34:19 +0100

    task: input search box component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ class App extends Component {
     this.state = {
       monsters: []
     }
+
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
+
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(
@@ -25,8 +29,18 @@ class App extends Component {
   }
 
   render() {
+    console.log('render')
+
     return (
       <div className="App">
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            console.log(event.target.value)
+          }}
+        />
         {
           this.state.monsters.map((monster) => {
             return(
